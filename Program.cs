d47a4a85c4eefea46d1defc116a7203b482fe87d
Perivska Zoreslava <130085130+zoriiaa@users.noreply.github.com>
2025-09-29 15:27:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,66 +1,33 @@
-﻿namespace Task5
+﻿namespace Task1
 {
-
- public class Program
-{
-    public static double GetAverage(int[] marks)
+    public class Program
     {
-        int sum = 0;
-        foreach (int mark in marks)
+
+        public static bool IsEven(int number)
         {
-            sum += mark;
+            return number % 2 == 0;
         }
-        return sum / marks.Length;
-    }
 
-    public static int GetMin(int[] marks)
-    {
-        int min = marks[0];
-        foreach (int mark in marks)
+        public static string GetMessage(int number)
         {
-            if (mark < min)
+            if (IsEven(number))
             {
-                min = mark;
+                return "Двері відкриваються!";
+            }
+            else
+            {
+                return "Двері зачинені...";
             }
         }
-        return min;
-    }
 
-    public static int GetMax(int[] marks)
-    {
-        int max = marks[0];
-        foreach (int mark in marks)
+        public static void Main()
         {
-            if (mark > max)
-            {
-                max = mark;
-            }
-        }
-        return max;
-    }
-
-    public static void PrintGroupStatistics(int[][] groups)
-    {
-        for (int i = 0; i < groups.Length; i++)
-        {
-            double avg = GetAverage(groups[i]);
-            int min = GetMin(groups[i]);
-            int max = GetMax(groups[i]);
-            Console.WriteLine($"Група {i + 1}: Середній = {avg:F0}, Мінімальний = {min}, Максимальний = {max}");
+            Console.WriteLine("Введіть число");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMessage(number));
         }
     }
 
-    public static void Main()
-    {
-
-        int[][] groups = new int[][]
-        {
-            new int[] { 60, 70, 80, 90, 100 },
-            new int[] { 50, 65, 75, 85, 95 },
-            new int[] { 90, 92, 94, 96, 98, 100 }
-        };
-
-        PrintGroupStatistics(groups);
-    }
 }
-}
+
+
